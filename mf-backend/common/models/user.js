313fc6b88499bce6ee4b52a79e14bca947251b96
colipsone <colipsone@gmail.com>
2016-05-04 23:22:29 +0300

    GetNearestEvents method refactored to retrieve friends evens along with self events and logic moved to userService separate class

--- mf-backend/common/models/user.js
+++ mf-backend/common/models/user.js
@@ -1,131 +1,44 @@
 module.exports = function (User) {
-	User.getNearestEvents = function(userId, callBack) {
-		User.findById(userId, {
-			include: {
-				relation: 'events',
-				scope: {
-					fields: {
-						title: true,
-						description: true,
-						type: true,
-						from: true,
-						id: true
-					}
-				}
-			}
-		}, (error, user) => {
-			if (error) {
-				throw error;
-			}
-			var events = user.events();
-			events.forEach((event) => {
-				event.photoUrl = user.photoUrl;
-				event.userName = user.username;
-			});
-			callBack(null, events);
+
+	var UserService = new require("../../server/services/userService");
+	var userServiceInst = new UserService(User);
+
+	User.getNearestEvents = function(userId, from, to, callback){
+		var fromDate = new Date(from);
+		var toDate = new Date(to);
+		userServiceInst.getNearestEvents(userId, fromDate, toDate).then(events => {
+				callback(null, events);
 		});
 	};
 
-	User.getFriends = function (userId, callBack) {
-	    User.findById(userId,
-	    {
-	        include: ['myFriends', 'friendTo']
-	    },
-	    (error, user) => {
-	        if (error) {
-	            throw error;
-	        }
-	        var friends = [];
-	        user.friendTo().forEach(function(friend) {
-	            friends.push(friend);
-	        });
-	        user.myFriends().forEach(function (friend) {
-	            friends.push(friend);
-	        });
-	        callBack(null, friends);
-	    });
+	User.getFriends = function (userId, callback) {
+		userServiceInst.getFriends(userId)
+		.then(friends => {
+			callback(null, friends);
+		});
 	};
 
-	User.addToFriends = function (firstUserId, secondUserId, callBack) {
-	    User.find({
-	        where: {
-	            id: { inq: [firstUserId, secondUserId] }
-	        },
-	        include: ['myFriends', 'friendTo']
-	    },
-	    (err, users) => {
-	        if (err) {
-	            callBack(null, `An error have been occured during operation! Details: ${err}`);
-	        }
-            if (users.length < 2) {
-                callBack(null, 'One of mentioned users haven\'t been found!');
-            }
-            /*var firstUserFriendsIds = users[0].myFriends().map(user => user['id']);
-            var secondUserFriendsIds = users[0].myFriends().map(user => user['id']);
-	        console.log(firstUserFriendsIds.find(function(id) { return id === secondUserId; }));
-	        console.log(secondUserFriendsIds.find(function (id) { return id === firstUserId; }));
-            if (firstUserFriendsIds.find(function(id) { return id === secondUserId; }) ||
-	            secondUserFriendsIds.find(function(id) { return id === firstUserId; })){
-	            callBack(null, `Users ${users[0].username} and ${users[1].username} are friends already!`);
-	        }*/
-	        users[0].myFriends.add(users[1], (err) => {
-	            if (err) {
-	                callBack(null, `An error have been occured during operation! Details: ${err}`);
-	            }
-	            callBack(null, `Users ${users[0].username} and ${users[1].username} are friends now!`);
-	        });
-	    });
+	User.addToFriends = function (firstUserId, secondUserId, callback) {
+		userServiceInst.addToFriends(firstUserId, secondUserId)
+		.then(result => {
+			callback(null, result);
+		});
 	};
 
-	User.removeFromFriends = function (firstUserId, secondUserId, callBack) {
-	    User.find({
-	        where: {
-	            id: { inq: [firstUserId, secondUserId] }
-	        },
-	        include: ['myFriends', 'friendTo']
-	    },
-	    (err, users) => {
-	        if (err) {
-	            callBack(null, `An error have been occured during operation! Details: ${err}`);
-	        }
-	        if (users.length < 2) {
-	            callBack(null, 'One of mentioned users haven\'t been found!');
-	        }
-	        users[0].myFriends.findById(secondUserId, (err, friend) => {
-	            if (err) {
-	                callBack(null, `An error have been occured during operation! Details: ${err}`);
-	            }
-	            if (friend) {
-	                users[0].myFriends.remove(friend,
-	                (err) => {
-	                    if (err) {
-	                        callBack(null, `An error have been occured during operation! Details: ${err}`);
-	                    }
-	                });
-	            } else {
-	                users[0].friendTo.findById(secondUserId,
-	                (err, friend) => {
-	                    if (err) {
-	                        callBack(null, `An error have been occured during operation! Details: ${err}`);
-	                    }
-	                    if (friend) {
-	                        users[0].friendTo.remove(friend,
-	                        (err) => {
-	                            if (err) {
-	                                callBack(null, `An error have been occured during operation! Details: ${err}`);
-	                            }
-	                        });
-	                    }
-	                });
-	            }
-	            callBack(null, `Users ${users[0].username} and ${users[1].username} are not friends anymore!`);
-	        });
-	    });
+	User.removeFromFriends = function (firstUserId, secondUserId, callback) {
+		userServiceInst.removeFromFriends(firstUserId, secondUserId)
+		.then(result => {
+			callback(null, result);
+		});
 	};
 
 	User.remoteMethod('getNearestEvents', {
 		http: {path: '/nearestEvents', verb: 'get'},
-		accepts: {arg: 'userId', type: 'string'},
+		accepts: [
+			{arg: 'userId', type: 'string'},
+			{arg: 'from', type: 'string'},
+			{arg: 'to', type: 'string'},
+		],
 		returns: {arg: 'events', type: 'Array'}
 	});
 
---
+++ mf-backend/server/services/userService.js
@@ -0,0 +1,201 @@
+var UserService = function(UserModel) {
+
+  var _ = require("underscore");
+
+  //#region Private Members
+
+  var getNearestEvents = function(userId, from, to) {
+    return new Promise((resolve, reject) => {
+      UserModel.findById(userId, {
+  				"include": [
+  					{
+  						"relation": "myFriends",
+  						"scope": {
+  							"include": {
+  								"relation": "events",
+  								"scope": {
+  									"where": {"and":
+  										[
+  											{"from": {"gte": from}},
+  											{"to": {"lte": to}}
+  										]
+  									}
+  								}
+  							}
+  						}
+  					},
+  					{
+  						"relation": "friendTo",
+  						"scope": {
+  							"include": {
+  								"relation": "events",
+  								"scope": {
+  									"where": {"and":
+  										[
+  											{"from": {"gte": from}},
+  											{"to": {"lte": to}}
+  										]
+  									}
+  								}
+  							}
+  						}
+  					},
+  					{
+  						"relation": "events",
+  						"scope": {
+  							"where": {"and":
+  								[
+  									{"from": {"gte": from}},
+  									{"to": {"lte": to}}
+  								]
+  							}
+  						}
+  					}
+  			]
+  		}, function(error, user) {
+  			if (error) {
+  				reject(error);
+  			}
+        //resolve(user);
+  			var primaryUserEvents = [],
+  					myFriendsEvents = [],
+  					friendToEvents = [];
+  			user.events().forEach(event => {
+  				event.photoUrl = user.photoUrl;
+  				event.userName = user.username;
+  				primaryUserEvents.push(event);
+  			});
+  			user.myFriends().forEach(friend => {
+  				friend.events().forEach(event => {
+  					event.photoUrl = friend.photoUrl;
+  					event.userName = friend.username;
+  					myFriendsEvents.push(event);
+  				});
+  			});
+  			user.friendTo().forEach(friend => {
+  				friend.events().forEach(event => {
+  					event.photoUrl = friend.photoUrl;
+  					event.userName = friend.username;
+  					friendToEvents.push(event);
+  				});
+  			});
+        var result = _.union(primaryUserEvents, myFriendsEvents, friendToEvents);
+  			resolve(_.sortBy(result, "from"));
+  		});
+    });
+  };
+
+  var getFriends = function (userId) {
+    return new Promise((resolve, reject) => {
+      UserModel.findById(userId,
+	    {
+	        include: ['myFriends', 'friendTo']
+	    },
+	    (error, user) => {
+	        if (error) {
+	            reject(error);
+	        }
+	        var friends = [];
+	        user.friendTo().forEach(function(friend) {
+	            friends.push(friend);
+	        });
+	        user.myFriends().forEach(function (friend) {
+	            friends.push(friend);
+	        });
+	        resolve(friends);
+	    });
+    });
+	};
+
+  var addToFriends = function(firstUserId, secondUserId) {
+    return new Promise((resolve, reject) => {
+      UserModel.find({
+          where: {
+            id: {
+              inq: [firstUserId, secondUserId]
+            }
+          },
+          include: ['myFriends', 'friendTo']
+        }, (err, users) => {
+          if (err) {
+            reject(`An error has been occured during operation! Details: ${err}`);
+          }
+          if (users.length < 2) {
+            reject('One of mentioned users hasn\'t been found!');
+          }
+          var firstUser = users[0],
+              secondUser = users[1],
+              firstUserFriends = _.union(firstUser.myFriends(), firstUser.friendTo());
+          if (_.find(firstUserFriends, user => {
+              return user.id.toString() == secondUser.id.toString();
+          })) {
+              resolve(`Users ${firstUser.username} and ${secondUser.username} are friends already!`);
+          }
+          firstUser.myFriends.add(secondUser, (err) => {
+            if (err) {
+              reject(`An error has been occured during operation! Details: ${err}`);
+            }
+            resolve(`Users ${firstUser.username} and ${secondUser.username} are friends now!`);
+          });
+        });
+    });
+  };
+
+  var removeFromFriends = function (firstUserId, secondUserId) {
+      return new Promise((resolve, reject) => {
+        UserModel.find({
+  	        where: {
+  	            id: { inq: [firstUserId, secondUserId] }
+  	        },
+  	        include: ['myFriends', 'friendTo']
+  	    },
+  	    (err, users) => {
+  	        if (err) {
+  	            reject(`An error has been occured during operation! Details: ${err}`);
+  	        }
+  	        if (users.length < 2) {
+  	            resolve('One of mentioned users hasn\'t been found!');
+  	        }
+            var firstUser = _.find(users, user => {return user.id.toString() == firstUserId}),
+                secondUser = _.find(users, user => {return user.id.toString() == secondUserId}),
+                firstUserFriends = firstUser.myFriends(),
+                firstUserFriendTo = firstUser.friendTo();
+                firstUserAllFriends = _.union(firstUserFriends, firstUserFriendTo),
+                friend = _.find(firstUserAllFriends, user => {
+                  return user.id.toString() == secondUserId;
+                });
+            if (friend) {
+              if (_.indexOf(firstUserFriends, friend) != -1) {
+                firstUser.myFriends.remove(friend, (err, user) => {
+                  if (err) {
+                    reject(err);
+                  }
+                  resolve(`Users ${firstUser.username} and ${secondUser.username} are not friends anymore!`);
+                });
+              } else {
+                firstUser.friendTo.remove(friend, (err, user) => {
+                  if (err) {
+                    reject(err);
+                  }
+                  resolve(`Users ${firstUser.username} and ${secondUser.username} are not friends anymore!`);
+                });
+              }
+            } else {
+              resolve(`Users ${firstUser.username} and ${secondUser.username} are not friends!`);
+            }
+        });
+    });
+  }
+
+  //#endregion
+
+  return {
+    getNearestEvents: getNearestEvents,
+    addToFriends: addToFriends,
+    getFriends: getFriends,
+    removeFromFriends: removeFromFriends
+  };
+
+}
+
+module.exports = UserService;
